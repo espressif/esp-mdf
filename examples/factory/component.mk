6930a75ee00b6aac9f23692a4b0e717c51b53b17
liyin <liyin@espressif.com>
2018-06-26 14:41:47 +0800

    Style(config): Move Kconfig file to components in esp-mdf directory

--- examples/factory/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-COMPONENT_SRCDIRS += espnow event_loop http_parse info_store json openssl socket upgrade
-COMPONENT_ADD_INCLUDEDIRS := espnow/include event_loop/include http_parse/include info_store/include json/include openssl/include socket/include upgrade/include
