75a78a641bf00fb69a7d0f3f57210b8692719471
Li Qing Qiao <liqingqiao@espressif.com>
2019-06-24 14:46:47 +0800

    feat: Add the menuconfig switch to compile the maliyun linkkit option

--- components/maliyun_linkkit/component.mk
+++ components/maliyun_linkkit/component.mk
@@ -1,4 +1,19 @@
 #
-# "main" pseudo-component makefile.
+# Component Makefile
 #
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+ifdef CONFIG_MESH_SUPPORT_ALIYUN_LINKKIT
+
+COMPONENT_ADD_INCLUDEDIRS := include
+
+COMPONENT_SRCDIRS := .
+
+# Check the submodule is initialised
+#COMPONENT_SUBMODULES :=
+
+else
+# Disable maliyun linkkit support
+COMPONENT_ADD_INCLUDEDIRS :=
+COMPONENT_ADD_LDFLAGS :=
+COMPONENT_SRCDIRS :=
+endif
