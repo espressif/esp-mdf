84bc43b8f111244f2a72e67846ba9442ffbace03
Zhan Zhao Chen <zhanzhaochen@espressif.com>
2019-07-06 13:41:32 +0800

    feat(mconfig_example): Example of improving network configuration

--- examples/function_demo/mconfig/main/mconfig_example.c
+++ examples/function_demo/mconfig/main/mconfig_example.c
@@ -87,10 +87,65 @@ static mdf_err_t event_loop_cb(mdf_event_loop_t event, void *ctx)
     return MDF_OK;
 }
 
+mdf_err_t get_network_config(const char *name, mwifi_config_t *mwifi_config, char custom_data[32])
+{
+    MDF_PARAM_CHECK(name);
+    MDF_PARAM_CHECK(mwifi_config);
+    MDF_PARAM_CHECK(custom_data);
+
+    mconfig_data_t *mconfig_data        = NULL;
+    mconfig_blufi_config_t blufi_config = {
+        .tid = 1, /**< Type of device. Used to distinguish different products,
+                       APP can display different icons according to this tid. */
+        .company_id = MCOMMON_ESPRESSIF_ID, /**< Company Identifiers (https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers) */
+    };
+
+    strncpy(blufi_config.name, name, sizeof(blufi_config.name) - 1);
+    MDF_LOGI("BLE name: %s", name);
+
+    /**
+     * @brief Initialize Bluetooth network configuration
+     */
+    MDF_ERROR_ASSERT(mconfig_blufi_init(&blufi_config));
+
+    /**
+     * @brief Network configuration chain slave initialization for obtaining network configuration information from master.
+     */
+    MDF_ERROR_ASSERT(mconfig_chain_slave_init());
+
+    /**
+     * @brief Get Network configuration information from blufi or network configuration chain.
+     *      When blufi or network configuration chain complete, will send configuration information to config_queue.
+     */
+    MDF_ERROR_ASSERT(mconfig_queue_read(&mconfig_data, portMAX_DELAY));
+
+    /**
+     * @brief Deinitialize Bluetooth network configuration and Network configuration chain.
+     */
+    MDF_ERROR_ASSERT(mconfig_chain_slave_deinit());
+    MDF_ERROR_ASSERT(mconfig_blufi_deinit());
+
+    memcpy(mwifi_config, &mconfig_data->config, sizeof(mwifi_config_t));
+    memcpy(custom_data, &mconfig_data->custom, sizeof(mconfig_data->custom));
+
+    /**
+     * @brief Switch to network configuration chain master mode to configure the network for other devices(slave), according to the white list.
+     */
+    if (mconfig_data->whitelist_size > 0) {
+        MDF_ERROR_ASSERT(mconfig_chain_master(mconfig_data, pdMS_TO_TICKS(60000)));
+    }
+
+    MDF_FREE(mconfig_data);
+
+    return MDF_OK;
+}
+
 void app_main()
 {
-    mconfig_data_t *mconfig_data        = NULL;
-    mconfig_blufi_config_t blufi_config = {0x0};
+    char name[28]        = {0x0};
+    char custom_data[32] = {0x0};
+    mwifi_config_t mwifi_config = {0x0};
+    mwifi_init_config_t init_config = MWIFI_INIT_CONFIG_DEFAULT();
 
     /**
      * @brief Set the log level for serial port printing.
@@ -108,62 +163,33 @@ void app_main()
     MDF_ERROR_ASSERT(mdf_event_loop_init(event_loop_cb));
     MDF_ERROR_ASSERT(wifi_init());
     MDF_ERROR_ASSERT(mespnow_init());
-    
-    /**
-     * @brief Network configuration chain slave initialization for obtaining network configuration information from master.
-     */
-    MDF_ERROR_ASSERT(mconfig_chain_slave_init());
 
     uint8_t sta_mac[6] = {0};
     MDF_ERROR_ASSERT(esp_wifi_get_mac(ESP_IF_WIFI_STA, sta_mac));
-    sprintf(blufi_config.name, "MESH_%02x%02x", sta_mac[4], sta_mac[5]);
-    MDF_LOGI("BLE name: %s", blufi_config.name);
+    sprintf(name, "ESP-MESH_%02x%02x", sta_mac[4], sta_mac[5]);
 
     /**
-     * @brief Initialize Bluetooth network configuration
+     * @note `custom_data` is used for specific application custom data, non-essential fields.
+     *       can be passed through the APP distribution network (ESP-MESH Config (The bottom
+     *       right corner of the configured page) > Custom Data)
      */
-    MDF_ERROR_ASSERT(mconfig_blufi_init(&blufi_config));
+    MDF_ERROR_ASSERT(get_network_config(name, &mwifi_config, custom_data));
 
-    /**
-     * @brief Get Network configuration information from blufi or network configuration chain.
-     *      When blufi or network configuration chain complete, will send configuration information to config_queue.
-     */
-    MDF_ERROR_ASSERT(mconfig_queue_read(&mconfig_data, portMAX_DELAY));
-    MDF_LOGI("mconfig, ssid: %s, password: %s, mesh_id: " MACSTR,
-             mconfig_data->config.router_ssid, mconfig_data->config.router_password,
-             MAC2STR(mconfig_data->config.mesh_id));
-
-    /**
-     * @brief Deinitialize Bluetooth network configuration and Network configuration chain.
-     */
-    MDF_ERROR_ASSERT(mconfig_chain_slave_deinit());
-    MDF_ERROR_ASSERT(mconfig_blufi_deinit());
+    MDF_LOGI("mconfig, ssid: %s, password: %s, mesh_id: " MACSTR ", custom: %.*s",
+             mwifi_config.router_ssid, mwifi_config.router_password,
+             MAC2STR(mwifi_config.mesh_id), sizeof(custom_data), custom_data);
 
     /**
      * @brief Note that once BT controller memory is released, the process cannot be reversed.
      *        It means you can not use the bluetooth mode which you have released by this function.
      *        it can release the .bss, .data and other section to heap
      */
-    MDF_ERROR_ASSERT(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+    MDF_ERROR_ASSERT(esp_bt_mem_release(ESP_BT_MODE_BLE));
 
     /**
      * @brief Initialize and start esp-mesh network according to network configuration information.
-     */    
-    MDF_ERROR_ASSERT(mwifi_init(&mconfig_data->init_config));
-    MDF_ERROR_ASSERT(mwifi_set_config(&mconfig_data->config));
-    MDF_ERROR_ASSERT(mwifi_start());
-
-    /**
-     * @brief Switch to network configuration chain master mode to configure the network for other devices(slave), according to the white list.
      */
-    if (mconfig_data->whitelist_size > 0) {
-        for (int i = 0; i < mconfig_data->whitelist_size / sizeof(mconfig_whitelist_t); ++i) {
-            MDF_LOGD("count: %d, data: " MACSTR,
-                     i, MAC2STR((uint8_t *)mconfig_data->whitelist_data + i * sizeof(mconfig_whitelist_t)));
-        }
-
-        MDF_ERROR_ASSERT(mconfig_chain_master(mconfig_data, 60000 / portTICK_RATE_MS));
-    }
-
-    MDF_FREE(mconfig_data);
+    MDF_ERROR_ASSERT(mwifi_init(&init_config));
+    MDF_ERROR_ASSERT(mwifi_set_config(&mwifi_config));
+    MDF_ERROR_ASSERT(mwifi_start());
 }
