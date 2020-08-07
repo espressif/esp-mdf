37677eb87ed28e18960735bddfedc8eee222ef34
zhanghuifeng <zhanghuifeng@espressif.com>
2020-08-07 17:12:35 +0800

    fix: fix coexist error
    1. fix coexist error
    2. fix strncpy issue in mlink
    3. add BLE_SMP_ENABLE=y to mconfig and light example

--- examples/development_kit/light/components/mesh_utils/mesh_utils.c
+++ examples/development_kit/light/components/mesh_utils/mesh_utils.c
@@ -120,7 +120,6 @@ mdf_err_t wifi_init()
     MDF_ERROR_ASSERT(esp_wifi_init(&cfg));
     MDF_ERROR_ASSERT(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
     MDF_ERROR_ASSERT(esp_wifi_set_mode(WIFI_MODE_STA));
-    MDF_ERROR_ASSERT(esp_wifi_set_ps(WIFI_PS_NONE));
     MDF_ERROR_ASSERT(esp_mesh_set_6m_rate(false));
     MDF_ERROR_ASSERT(esp_wifi_start());
 
